2e781f9f7a50e8b25a0bd18d0ca566b66caa4dda
dyhalmeida <dyhalmeida@gmail.com>
2024-03-30 16:46:37 -0300

    chore: add constants to server action get photos

--- src/server-actions/photos/get-photos.ts
+++ src/server-actions/photos/get-photos.ts
@@ -1,9 +1,10 @@
 'use server'
 
+import { BASE_URL, RESOURCE } from "@/constants/api"
 import { IPhoto } from "../interfaces/photo.interface"
 
 export const getPhotos = async (): Promise<IPhoto[]> => {
-    const response = await fetch('https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0')
+    const response = await fetch(`${BASE_URL}/${RESOURCE.PHOTOS}`)
     const data = await response.json()
     return data as IPhoto[]
 }
