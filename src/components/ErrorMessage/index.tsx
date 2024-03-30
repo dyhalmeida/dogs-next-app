82870cefec0a57f61567ca532024865eba1a0867
dyhalmeida <dyhalmeida@gmail.com>
2024-03-30 17:16:59 -0300

    feat: add error message component

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,13 @@
+export const ErrorMessage = ({ error }: { error?: string }) => {
+  if (!error) return null
+  return (
+    <p
+      style={{
+        color: '#f31',
+        margin: '1rem 0',
+      }}
+    >
+      {error}
+    </p>
+  )
+}
