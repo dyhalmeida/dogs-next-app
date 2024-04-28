81c82d20c8bc2a60d346d82e9a69d2332e1ed576
Diego Almeida <dyhalmeida@gmail.com>
2024-04-28 17:37:54 -0300

    feat: add server action get user

---
+++ src/server-actions/login/get-user.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import { BASE_URL, RESOURCE } from '@/constants/api'
+import { IUser } from '@/context/user-context'
+import { ApiError } from '@/utils/api-error'
+import { cookies } from 'next/headers'
+
+export const getUser = async () => {
+  try {
+    const token = cookies().get('token')?.value
+    if (!token) throw new Error('token not provided')
+    const response = await fetch(`${BASE_URL}/${RESOURCE.USER}`, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+      next: {
+        revalidate: 60,
+      },
+    })
+    if (!response.ok) throw new Error('user not found')
+    const data = (await response.json()) as IUser
+    return { data, ok: true, error: '' }
+  } catch (error: unknown) {
+    return ApiError(error)
+  }
+}
