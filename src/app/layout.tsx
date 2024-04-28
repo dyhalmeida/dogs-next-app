7dc2dbff43584459fa1b5b0fea9b18ed82e5ccf9
Diego Almeida <dyhalmeida@gmail.com>
2024-04-28 17:38:31 -0300

    feat: add user context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,24 +3,30 @@ import { spectral } from '@/fonts'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import './globals.css'
+import { UserContextProvider } from '@/context/user-context'
+import { getUser } from '@/server-actions/login/get-user'
 
 export const metadata: Metadata = {
   title: 'Dogs App',
   description: 'Rede Social para cachorros',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const { data: user } = await getUser()
+
   return (
     <html lang="pt-br">
       <body className={spectral.variable}>
         <div className="App">
-          <Header />
-          <main className="AppBody">{children}</main>
-          <Footer />
+          <UserContextProvider user={user}>
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </UserContextProvider>
         </div>
       </body>
     </html>
---
+++ src/context/user-context.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React from 'react'
+
+export interface IUser {
+  id: number
+  nome: string
+  username: string
+  email: string
+}
+
+interface IUserContext {
+  user: IUser | null
+  setUser: (user: IUser) => void
+}
+
+const UserContext = React.createContext<IUserContext | null>(null)
+
+export const useUser = () => {
+  const context = React.useContext(UserContext)
+
+  if (!context) {
+    throw new Error('UserContextProvider not found')
+  }
+
+  return context
+}
+
+export function UserContextProvider({
+  children,
+  user,
+}: {
+  user: IUser | null
+  children: React.ReactNode
+}) {
+  const [userState, setUserState] = React.useState<IUser | null>(user)
+
+  const setUser = (user: IUser) => {
+    setUserState((prevData) => ({ ...prevData, ...user }))
+  }
+
+  const value = {
+    user: userState,
+    setUser,
+  }
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
