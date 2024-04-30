862137c39294759b4d2dfd10ba572861d9634c04
Diego Almeida <dyhalmeida@gmail.com>
2024-04-30 19:18:54 -0300

    feat: add hook use media

---
+++ src/hooks/use-media.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+export const useMedia = (media: string) => {
+  const [match, setMatch] = useState<boolean>(false)
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media)
+      setMatch(matches)
+    }
+    changeMatch()
+    window.addEventListener('resize', changeMatch)
+    return () => {
+      window.removeEventListener('resize', changeMatch)
+    }
+  }, [media])
+
+  return match
+}
