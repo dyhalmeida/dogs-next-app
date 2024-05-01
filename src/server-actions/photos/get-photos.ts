8e458e5412a776c856e7333f2df24ddbeab6b117
Diego Almeida <dyhalmeida@gmail.com>
2024-05-01 16:20:21 -0300

    refactor: changes server action get photos

--- src/server-actions/photos/get-photos.ts
+++ src/server-actions/photos/get-photos.ts
@@ -1,10 +1,15 @@
 'use server'
 
-import { BASE_URL, RESOURCE } from "@/constants/api"
-import { IPhoto } from "../interfaces/photo.interface"
+import { BASE_URL, RESOURCE } from '@/constants/api'
+import { IPhoto } from '../interfaces/photo.interface'
 
 export const getPhotos = async (): Promise<IPhoto[]> => {
-    const response = await fetch(`${BASE_URL}/${RESOURCE.PHOTOS}`)
-    const data = await response.json()
-    return data as IPhoto[]
+  const response = await fetch(
+    `${BASE_URL}/${RESOURCE.PHOTOS}/?_page=1&_total=6&_user=0`,
+    {
+      next: { revalidate: 10, tags: ['photos'] },
+    },
+  )
+  const data = await response.json()
+  return data as IPhoto[]
 }
