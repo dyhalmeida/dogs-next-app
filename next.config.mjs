21b989f49bdb337acd4e9a0b9e02da1814e3dc8d
dyhalmeida <dyhalmeida@gmail.com>
2024-06-15 09:46:07 -0300

    feat(component): add photo content

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'dogsapi.origamid.dev',
-            }
-        ]
-    }
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dogsapi.origamid.dev',
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+}
 
-export default nextConfig;
+export default nextConfig
---
+++ src/components/PhotoContent/index.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React from 'react'
+import styles from './PhotoContent.module.css'
+import Link from 'next/link'
+import { useUser } from '@/context/user-context'
+import Image from 'next/image'
+import { IPhotoData } from '@/server-actions/photos/get-photo'
+
+const PhotoContent = ({
+  data,
+  single,
+}: {
+  data: IPhotoData
+  single: boolean
+}) => {
+  const { user } = useUser()
+  const { photo } = data
+
+  return (
+    <div className={`${styles.photo} ${single ? styles.single : ''}`}>
+      <div className={styles.img}>
+        <Image src={photo.src} alt={photo.title} width={1000} height={1000} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user && user.username === photo.author ? (
+              <p>Deletar</p>
+            ) : (
+              <Link href={`/profile/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.views}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link href={`/photo/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PhotoContent
