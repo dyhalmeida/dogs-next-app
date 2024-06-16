49948563f076fb2573f2aa5b0ebc7f677371578a
dyhalmeida <dyhalmeida@gmail.com>
2024-06-16 10:22:55 -0300

    feat: add profile page

--- src/app/profile/[user]/page.tsx
+++ src/app/profile/[user]/page.tsx
@@ -1,12 +1,22 @@
-interface IUserProfileProps {
-  params: {
-    user: string
-  }
-}
-export default function UserProfilePage({ params }: IUserProfileProps) {
+import { Feed } from '@/components/Feed'
+import { getPhotos } from '@/server-actions/photos/get-photos'
+import { notFound } from 'next/navigation'
+
+export default async function PerfilUserPage({
+  params,
+}: {
+  params: { user: string }
+}) {
+  const { data } = await getPhotos({ user: params.user })
+
+  if (!data) return notFound()
+
+  const title = params.user.charAt(0).toUpperCase() + params.user.slice(1)
+
   return (
-    <div>
-      <h1>UserProfilePage: {params.user}</h1>
-    </div>
+    <section className="container mainSection">
+      <h1 className="title">{title}</h1>
+      <Feed photos={data} user={params.user} />
+    </section>
   )
 }
